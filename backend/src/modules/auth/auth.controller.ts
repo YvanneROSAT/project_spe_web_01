eff17cd39ec98ce4c968aad6b44a5a805eaa6c21
Lucien <lucien.neuhoff@protonmail.com>
2025-07-22 15:09:52 +0200

    ref: backend auth handler rework (more functional ?)

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,55 +1,53 @@
 import { Request, Response, Router } from "express";
+import z from "zod";
 import { AuthService } from "./auth.service";
-import { authInput } from "./dto/auth.input";
+import { AuthInput, authInput as authInputSchema } from "./dto/auth.input";
 import { InvalidCredentialsError, UserAlreadyExistsError } from "./errors";
+
+function makeHandler<T extends Record<string, unknown>>(
+  useCase: (data: T) => Promise<unknown>,
+  schema: z.ZodObject,
+  successStatus: number,
+  errorMap: [new () => Error, number][]
+) {
+  return async function handler(req: Request, res: Response) {
+    const { data, success } = schema.safeParse(req.body);
+    if (!success) {
+      return res.sendStatus(422);
+    }
+
+    try {
+      const result = await useCase(data as T);
+
+      return result
+        ? res.status(successStatus).json(result)
+        : res.sendStatus(successStatus);
+    } catch (err) {
+      for (const [Err, status] of errorMap) {
+        if (err instanceof Err) {
+          return res.sendStatus(status);
+        }
+      }
+
+      return res.sendStatus(500);
+    }
+  };
+}
 
 export function AuthController() {
   const authService = AuthService();
 
-  async function handleLogin(req: Request, res: Response) {
-    const { email, password } = req.body;
-
-    const { data, success } = authInput.safeParse({ email, password });
-    if (!success) {
-      return res.sendStatus(422);
-    }
-
-    try {
-      const token = await authService.login(data);
-
-      res.json({ token });
-    } catch (err) {
-      if (err instanceof InvalidCredentialsError) {
-        return res.sendStatus(401);
-      }
-
-      return res.sendStatus(500);
-    }
-  }
-
-  async function handleRegister(req: Request, res: Response) {
-    const { data, success } = authInput.safeParse({
-      email: req.body.email,
-      password: req.body.password,
-    });
-    if (!success) {
-      return res.sendStatus(422);
-    }
-
-    try {
-      await authService.register(data);
-
-      res.sendStatus(201);
-    } catch (err) {
-      if (err instanceof UserAlreadyExistsError) {
-        return res.sendStatus(403);
-      }
-
-      return res.sendStatus(500);
-    }
-  }
-
   return Router()
-    .get("/auth/login", handleLogin)
-    .get("/auth/register", handleRegister);
+    .get(
+      "/auth/login",
+      makeHandler<AuthInput>(authService.login, authInputSchema, 200, [
+        [InvalidCredentialsError, 401],
+      ])
+    )
+    .get(
+      "/auth/register",
+      makeHandler<AuthInput>(authService.register, authInputSchema, 200, [
+        [UserAlreadyExistsError, 403],
+      ])
+    );
 }
