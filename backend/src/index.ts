833c0fee3591ea1dd883b909c1e302b2b682e3e8
Lucien <lucien.neuhoff@protonmail.com>
2025-07-22 16:29:30 +0200

    wip: backend rework

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,10 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import { JwtPayload } from "jsonwebtoken";
+import authRouter from "./router/auth.router";
 
 const app = express();
-app.use(helmet()).use(cors()).use(express.json()).use();
+app.use(helmet()).use(cors()).use(express.json()).use(authRouter);
 
 app.listen(process.env.PORT);
 
