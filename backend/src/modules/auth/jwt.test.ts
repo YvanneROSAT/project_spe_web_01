97c324d981d7d8c526afef66256eda9a941af2f1
Lucien <lucien.neuhoff@protonmail.com>
2025-07-24 00:44:04 +0200

    wip: rewriting tests

--- backend/src/modules/auth/jwt.test.ts
+++ backend/src/modules/auth/jwt.test.ts
@@ -1,15 +1,57 @@
+import jwt from "jsonwebtoken";
 import { describe, expect, it } from "vitest";
-import { generateAccessToken } from "./jwt";
+import z from "zod";
+import { generateAccessToken, generateRefreshToken, verifyToken } from "./jwt";
 
-describe("generateJWToken", () => {
-  it("returns the signed the payload", () => {
-    process.env.ACCESS_TOKEN_SECRET = "jwt-secret";
+describe("generateAccessToken", () => {
+  it("returns the signed access token ", () => {
+    process.env.ACCESS_TOKEN_SECRET = "jwt-access-secret";
 
-    const res = generateAccessToken({
-      userId: "userId",
-      email: "john.smith@mail.com",
+    const res = generateAccessToken("userId");
+
+    expect(jwt.decode(res)).toEqual({
+      sub: "userId",
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
-
-    expect(res).toBeDefined();
   });
 });
+
+describe("generateRefreshToken", () => {
+  it("returns the signed refresh token ", () => {
+    process.env.REFRESH_TOKEN_SECRET = "jwt-refresh-secret";
+
+    const res = generateRefreshToken();
+
+    expect(jwt.decode(res)).toEqual({
+      sessionId: expect.any(String),
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
+  });
+});
+
+describe("verifyToken", () => {
+  const mSecret = "secret";
+  const mSchema = z.object({
+    sub: z.string(),
+  });
+
+  it("should return decoded payload for valid token", () => {
+    const mToken = jwt.sign({ sub: "subject" }, mSecret);
+
+    const res = verifyToken(mToken, mSchema, mSecret);
+
+    expect(res).toEqual({
+      sub: "subject",
+    });
+  });
+
+  it("should return null for invalid token", () => {
+    const res = verifyToken("invalid token", mSchema, mSecret);
+
+    expect(res).toEqual(null);
+  });
+});
+
+describe("refreshTokens", () => {});
