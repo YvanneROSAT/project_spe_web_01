847fdbaa0ad7929f342022051d9478b77cab0556
Lucien <lucien.neuhoff@protonmail.com>
2025-07-23 09:55:37 +0200

    ref: reverse auth router callback hell

--- backend/src/router/auth.router.ts
+++ backend/src/router/auth.router.ts
@@ -8,6 +8,9 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import z from "zod";
 
+const FAKE_PASSWORD_HASH =
+  "$2b$10$CwTycUXWue0Thq9StjUM0uJ8eDl3pZAgUQmrc9hUlPZx4s8ZfrfG2";
+
 export default Router()
   .post(
     "/auth/login",
@@ -18,27 +21,24 @@ export default Router()
       })
     ),
     async function (req, res) {
-      db.select()
+      const [user] = await db
+        .select()
         .from(usersTable)
-        .where(eq(usersTable.email, req.body.email))
-        .then(([user]) => {
-          // todo: how do we avoid timing attacks ? we should return only after comparing the password, even if the user isn't found
-          if (!user) {
-            return res.sendStatus(403);
-          }
+        .where(eq(usersTable.email, req.body.email));
 
-          bcrypt
-            .compare(req.body.password, user.passwordHash)
-            .then((passwordMatch) => {
-              if (!passwordMatch) {
-                return res.sendStatus(403);
-              }
+      const passwordMatch = await bcrypt.compare(
+        req.body.password,
+        // an attacker could guess if a user is registered based on the response time
+        // so we'll compare passwords even if the user isn't found to avoid timing attacks
+        user?.passwordHash ?? FAKE_PASSWORD_HASH
+      );
+      if (!passwordMatch) {
+        return res.sendStatus(403);
+      }
 
-              return res.json({
-                token: jwt.sign({ email: user.email }, JWT_SECRET),
-              });
-            });
-        });
+      return res.json({
+        token: jwt.sign({ email: user.email }, JWT_SECRET),
+      });
       // todo: catch errors
     }
   )
@@ -53,33 +53,30 @@ export default Router()
       })
     ),
     async function (req, res) {
-      db.select()
+      const [user] = await db
+        .select()
         .from(usersTable)
-        .where(eq(usersTable.email, req.body.email))
-        .then(([user]) => {
-          if (user) {
-            return res.sendStatus(403);
-          }
+        .where(eq(usersTable.email, req.body.email));
 
-          bcrypt
-            .hash(req.body.password, BCRYPT_SALT_ROUNDS)
-            .then((passwordHash) => {
-              db.insert(usersTable)
-                .values({
-                  firstName: req.body.firstName,
-                  lastName: req.body.lastName,
-                  email: req.body.email,
-                  passwordHash,
-                })
-                .then(([newUser]) => {
-                  if (!newUser) {
-                    return res.sendStatus(500);
-                  }
+      if (user) {
+        return res.sendStatus(403);
+      }
 
-                  return res.status(200);
-                });
-            });
-        });
+      const passwordHash = await bcrypt.hash(
+        req.body.password,
+        BCRYPT_SALT_ROUNDS
+      );
+      const [newUser] = await db.insert(usersTable).values({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        passwordHash,
+      });
+      if (!newUser) {
+        return res.sendStatus(500);
+      }
+
+      return res.status(200);
       // todo: catch errors
     }
   );
