0052b945d81cf8f4db391e8bb51028062f959e1b
Lucien <lucien.neuhoff@protonmail.com>
2025-07-25 23:51:16 +0200

    updated auth schemas

--- common/src/dto/auth.ts
+++ common/src/dto/auth.ts
@@ -2,10 +2,17 @@ import z from "zod";
 
 const emailField = z.email({ pattern: z.regexes.rfc5322Email }).toLowerCase();
 const passwordField = z
-  .string({ error: "Invalid password" })
-  .min(8, "Must be 8 characters or more");
-const firstNameField = z.string();
-const lastNameField = z.string();
+  .string({ error: "Mot de passe invalide" })
+  .min(8, "Doit faire au moins 8 caracteres")
+  .max(64, "Doit faire au plus 64 caracteres");
+const firstNameField = z
+  .string()
+  .min(2, "Prenom trop court")
+  .max(50, "Prenom trop long");
+const lastNameField = z
+  .string()
+  .min(2, "Nom trop court")
+  .max(50, "Nom trop long");
 
 export const loginSchema = z.object({
   email: emailField,
